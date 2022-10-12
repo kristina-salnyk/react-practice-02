9b6e3008ff0bef605d2dddd6bb736b07c94576a2
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-10-12 15:45:46 +0200

    Added form elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import { PageTitle } from 'components/PageTitle/PageTitle';
 import { EventBoard } from 'components/EventBoard/EventBoard';
+import { LoginForm } from 'components/LoginForm';
 import upcomingEvents from '../upcoming-events.json';
 import { Container } from './App.styled';
 import { Box } from './Box/Box';
+import { SignUpForm } from './SignUpForm';
 
 export const App = () => {
   return (
-    <Box bg="primaryBg" color="darkBlue" as="main">
-      <Container>
-        <PageTitle text="24th Core Worlds Coalition Conference" />
-        <EventBoard events={upcomingEvents} />
-      </Container>
-    </Box>
+    <>
+      <Box bg="primaryBg" color="darkBlue" as="main">
+        <Container>
+          <PageTitle text="24th Core Worlds Coalition Conference" />
+          <EventBoard events={upcomingEvents} />
+        </Container>
+      </Box>
+      <LoginForm onSubmit={values => console.log(values)} />
+      <SignUpForm onSubmit={values => console.log(values)} />
+    </>
   );
 };
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,124 @@
+import { Component } from 'react';
+
+const Gender = {
+  MALE: 'male',
+  FEMALE: 'female',
+};
+
+const INITIAL_STATE = {
+  login: '',
+  email: '',
+  password: '',
+  agreed: false,
+  gender: Gender.MALE,
+  age: '',
+};
+
+export class SignUpForm extends Component {
+  state = { ...INITIAL_STATE };
+
+  handleChange = evt => {
+    const { name, value, type, checked } = evt.target;
+    this.setState({ [name]: type === 'checkbox' ? checked : value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const { login, email, password, agreed, gender, age } = this.state;
+    console.log(
+      `Login: ${login}, Email: ${email}, Password: ${password}, Agreed: ${agreed}, Gender: ${gender}, Age: ${age}`
+    );
+    this.props.onSubmit({ ...this.state });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  render() {
+    const { login, email, password, agreed, gender, age } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="Enter login"
+            name="login"
+            value={login}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Agree to terms
+          <input
+            type="checkbox"
+            name="agreed"
+            checked={agreed}
+            onChange={this.handleChange}
+          />
+        </label>
+        <section>
+          <h2>Choose your gender</h2>
+          <label>
+            Male
+            <input
+              type="radio"
+              checked={gender === Gender.MALE}
+              name="gender"
+              value={Gender.MALE}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Female
+            <input
+              type="radio"
+              checked={gender === Gender.FEMALE}
+              name="gender"
+              value={Gender.FEMALE}
+              onChange={this.handleChange}
+            />
+          </label>
+        </section>
+        <label>
+          Choose your age
+          <select name="age" value={age} onChange={this.handleChange}>
+            <option value="" disabled>
+              ...
+            </option>
+            <option value="18-25">18-25</option>
+            <option value="26-35">26-35</option>
+            <option value="36+">36+</option>
+          </select>
+        </label>
+
+        <button type="submit" disabled={!agreed}>
+          Sign up as {login}
+        </button>
+      </form>
+    );
+  }
+}
